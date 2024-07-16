14f854603d3e7484feafe1c345adb5840e18b8ac
Vaishnavi Goswami <ce22btech11051@iith.ac.in>
2024-07-17 01:17:56 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path"
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
@@ -25,6 +26,15 @@ try {
 // Define routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+//deployment
+if(process.env.NODE.ENV==='production'){
+const dirPath=path.resolve();
+app.use(express.static("Frontend/dist"))
+
+app.get("*",(req,res)=>{
+res.sendFile(path.resolve(dirPath,'Frontend','dist','index.html'))
+})
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
